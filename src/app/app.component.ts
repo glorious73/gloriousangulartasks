0dacf343d6345208fb88465de073c83cd32e9945
glorious73 <amjadmh73@gmail.com>
2022-08-26 21:29:03 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gloriousangulartasks';
+  title: string = 'Glorious Task Tracker';
 }
