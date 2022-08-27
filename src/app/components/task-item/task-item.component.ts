dae1ac6e3c65fac4ed0c93f6c7c573c08c5a175b
glorious73 <amjadmh73@gmail.com>
2022-08-27 14:34:49 +0300

    Toggle task reminder done

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -11,7 +11,8 @@ export class TaskItemComponent implements OnInit {
 
   @Input() task!: Task
   @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
-  
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
+
   faTimes = faTimes;
 
   constructor() { }
@@ -22,5 +23,10 @@ export class TaskItemComponent implements OnInit {
   onDelete(task: Task) {
     this.onDeleteTask.emit(task);
   }
+
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);
+  }
+
 
 }
